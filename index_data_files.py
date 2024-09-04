d2331e8a70179c8831d90e328ed4d5cbced9e497
RobertPoncelet <robert.poncelet@talk21.com>
2024-09-04 01:50:59 +0100

    IndexFile -> AssetIndex, DataFile -> AssetData, a fuckton more refactoring

--- index_data_files.py
+++ index_data_files.py
@@ -6,21 +6,22 @@ import subprocess
 from multiprocessing import Pool, cpu_count
 
 
-UNKNOWN_NAME = "_UNKNOWN"
-
-
 class FileEntry:
     def __init__(self, file_entry=None, filename=None):
         self.file_entry = file_entry or (0, 0, 0, 0, 0)  # default 5-tuple
         self.filename = filename or ""
-        self.offset = self.file_entry[1]  # Second number is the offset
-        self.compressed = bool(self.file_entry[4])
+    
+    def offset(self):
+        return self.file_entry[1]  # Second number is the offset
 
-    def serialize(self):
+    def is_compressed(self):
+        return bool(self.file_entry[4])
+
+    def write_to_file(self):
         # Serialize the file entry and filename
         packed_entry = struct.pack('<IIIHH', *self.file_entry)
         filename_bytes = self.filename.encode('ascii') + b'\x00'
-        # TODO: surely the padding can't be right if we don't know where we are in the file
+        # TODO: surely the padding length can't be right if we don't know where we are in the file
         padding_length = (4 - (len(filename_bytes) % 4)) % 4
         filename_bytes += b'\x00' * padding_length
         return packed_entry + filename_bytes
@@ -39,27 +40,32 @@ class FileEntry:
         return cls(file_entry=file_entry, filename=filename), index
 
 
-class DataSection:
+class IndexSection:
     WEIRD_SECTION_TYPE = 0x0C
+    UNKNOWN_SECTION_NAME = "_UNKNOWN"
 
-    def __init__(self, section_type=0, unknown=0, section_name="", entries=None):
+    def __init__(self, section_type=0, unknown=0, section_name=None, entries=None):
         self.section_type = section_type
         self.unknown = unknown
-        self.section_name = section_name
+        self._section_name = section_name
         self.entries = entries or []
+    
+    def extracted_dir_name(self):
+        return self._section_name or self.UNKNOWN_SECTION_NAME
 
-    def serialize(self):
+    def write_to_file(self):
         if self.section_type == self.WEIRD_SECTION_TYPE:
             packed_header = struct.pack('<III', self.section_type, len(self.entries), self.unknown)
             section_name_bytes = b""
         else:
+            assert self._section_name
             packed_header = struct.pack('<III', self.section_type, self.unknown, len(self.entries))
-            section_name_bytes = self.section_name.encode('ascii') + b'\x00'
+            section_name_bytes = self._section_name.encode('ascii') + b'\x00'
             padding_length = (4 - (len(section_name_bytes) % 4)) % 4
             section_name_bytes += b'\x00' * padding_length
 
         # Serialize each file entry
-        entry_data = b''.join(entry.serialize() for entry in self.entries)
+        entry_data = b''.join(entry.write_to_file() for entry in self.entries)
         return packed_header + section_name_bytes + entry_data
 
     @classmethod
@@ -70,6 +76,7 @@ class DataSection:
 
         if section_type == cls.WEIRD_SECTION_TYPE:
             num_entries, unknown = struct.unpack_from('<II', data, index)
+            section_name = None
             index += 8
         else:
             unknown, num_entries = struct.unpack_from('<II', data, index)
@@ -89,15 +96,15 @@ class DataSection:
         return cls(section_type=section_type, unknown=unknown, section_name=section_name, entries=entries), index
 
 
-class IndexFile:
+class AssetIndex:
     INDEX_HEADER = b'\xFC\xF5\x02\x00\x10\x00\x00\x00'
 
     def __init__(self, sections=None):
         self.sections = sections or []
 
-    def serialize(self):
+    def write_to_file(self):
         # Serialize all sections
-        return self.INDEX_HEADER + b''.join(section.serialize() for section in self.sections)
+        return self.INDEX_HEADER + b''.join(section.write_to_file() for section in self.sections)
 
     @classmethod
     def read_from_file(cls, file_path):
@@ -107,45 +114,47 @@ class IndexFile:
         sections = []
         index = len(cls.INDEX_HEADER)  # Skip the first 8 bytes (header)
         while index < len(data):
-            section, index = DataSection.read_from_file(data, index)
+            section, index = IndexSection.read_from_file(data, index)
             sections.append(section)
 
         return cls(sections=sections)
 
 
-class DataFile:
-    def __init__(self, idx_file, dat_file_path):
-        self.idx_file = idx_file
+class AssetData:
+    def __init__(self, asset_index: AssetIndex, dat_file_path: str):
+        self.asset_index = asset_index
         self.dat_file_path = dat_file_path
 
     def extract_files(self, output_dir):
         os.makedirs(output_dir, exist_ok=True)
-        metadata = {}
 
+        sections_metadata = []
         tasks = []
 
-        for section in self.idx_file.sections:
-            section_dir = os.path.join(output_dir, section.section_name)
+        for section in self.asset_index.sections:
+            section_dir = os.path.join(output_dir, section.extracted_dir_name())
             os.makedirs(section_dir, exist_ok=True)
 
-            metadata[section.section_name] = {
-                'type': section.section_type,
-                'unknown': section.unknown,
-                'entries': []
-            }
+            entries_metadata = []
 
             for entry in section.entries:
-                offset = entry.offset
                 filename = entry.filename
                 output_file_path = os.path.join(section_dir, filename)
 
-                tasks.append((self.dat_file_path, output_file_path, offset))
+                tasks.append((self.dat_file_path, output_file_path, entry.offset()))
 
                 # Collect metadata needed to reconstruct the idx file
-                metadata[section.section_name]['entries'].append({
+                entries_metadata.append({
                     'filename': filename,
                     'file_entry': entry.file_entry
                 })
+            
+            sections_metadata.append({
+                'name': section.section_name,
+                'type': section.section_type,
+                'unknown': section.unknown,
+                'entries': entries_metadata
+            })
 
         # Run extraction tasks in parallel using multiprocessing
         with Pool(cpu_count()) as pool:
@@ -153,71 +162,61 @@ class DataFile:
 
         # Save metadata to JSON
         with open(os.path.join(output_dir, 'metadata.json'), 'w') as json_file:
-            json.dump(metadata, json_file, indent=4)
+            json.dump(sections_metadata, json_file, indent=4)
 
     def _extract_file(self, args):
         dat_file_path, output_file_path, offset = args
         command = [
             'rnc_lib.exe', 'u', dat_file_path, output_file_path, f'-i={offset:X}'
         ]
-        subprocess.check_call(command, check=True)
+        subprocess.check_call(command)
         print(f'Extracted {os.path.basename(output_file_path)} to {output_file_path}.')
 
-    def pack_files(self, output_dir):
-        with open(os.path.join(output_dir, 'metadata.json'), 'r') as json_file:
-            metadata = json.load(json_file)
+    def pack_files(self, asset_dir):
+        for section in self.asset_index.sections:
+            section_dir = os.path.join(asset_dir, section.extracted_dir_name())
 
-        for section_name, section_data in metadata.items():
-            section_dir = os.path.join(output_dir, section_name)
-
-            for entry_data in section_data['entries']:
-                filename = entry_data['filename']
-                input_file_path = os.path.join(section_dir, filename)
-                offset = entry_data['file_entry'][1]
+            for entry in section.entries:
+                input_file_path = os.path.join(section_dir, entry.filename)
 
                 temp_dat_path = input_file_path + '.dat'
                 command = [
                     'rnc_lib.exe', 'p', input_file_path, temp_dat_path, '-m=1'
                 ]
-                subprocess.check_call(command, check=True)
+                subprocess.check_call(command)
 
                 with open(temp_dat_path, 'rb') as temp_file:
                     packed_data = temp_file.read()
 
                 with open(self.dat_file_path, 'r+b') as dat_file:
-                    dat_file.seek(offset)
+                    dat_file.seek(entry.offset())
                     dat_file.write(packed_data)
 
                 os.remove(temp_dat_path)
 
-                print(f'Packed {filename} to {self.dat_file_path} at offset {offset:X}.')
+                print(f'Packed {entry.filename} to {self.dat_file_path} at offset {entry.offset():X}.')
 
-    def create_idx_file(self, output_path):
-        with open(os.path.join(output_path, 'metadata.json'), 'r') as json_file:
+    @classmethod
+    def from_asset_dir(cls, asset_dir, dat_file_path):
+        with open(os.path.join(asset_dir, 'metadata.json'), 'r') as json_file:
             metadata = json.load(json_file)
 
         sections = []
-        for section_name, section_data in metadata.items():
+        for section_data in metadata:
             entries = [
                 FileEntry(file_entry=entry['file_entry'], filename=entry['filename'])
                 for entry in section_data['entries']
             ]
-            section = DataSection(
+            section = IndexSection(
                 section_type=section_data['type'],
                 unknown=section_data['unknown'],
-                section_name=section_name,
+                section_name=section_data['name'],
                 entries=entries
             )
             sections.append(section)
 
-        idx_file = IndexFile(sections)
-        with open(output_path, 'wb') as f:
-            f.write(idx_file.serialize())
-
-
-# BASE_DIR = "F:/Google Drive/RW3/assets/"
-# INDEX_FILE = "lumpy.idx"
-# DATA_FILE = "lumpy.dat"
+        asset_index = AssetIndex(sections)
+        return cls(asset_index, dat_file_path)
 
 
 if __name__ == "__main__":
@@ -239,8 +238,13 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.command == "unpack":
-        process_files('read', args.index_file, args.data_file, args.output_dir)
+        asset_index = AssetIndex.read_from_file(args.index_file)
+        asset_data = AssetData(asset_index, args.data_file)
+        asset_data.extract_files(args.output_dir)
     elif args.command == "pack":
-        process_files('write', args.index_file, args.data_file, args.asset_dir)
+        asset_data = AssetData.from_asset_dir(args.asset_dir, args.data_file)
+        asset_data.pack_files()
+        with open(args.index_file, 'wb') as f:
+            f.write(asset_data.asset_index.write_to_file())
     else:
         raise argparse.ArgumentError("Unknown command!")
