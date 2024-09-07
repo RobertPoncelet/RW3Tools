3baf0eae112813b0e73e80bb8e627a5353a67138
RobertPoncelet <robert.poncelet@talk21.com>
2024-09-07 20:02:05 +0100

    Allow reading from OBJs and refactor a bit before doing the writing to .rwm

--- mesh.py
+++ mesh.py
@@ -2,108 +2,114 @@ import sys, os, struct
 
 
 def read_string(file):
-    string_size = struct.unpack_from('<I', file.read(4))[0]
+    string_size = struct.unpack('<I', file.read(4))[0]
     return file.read(string_size).decode('ascii')
-
 
 def read_strings(file, num):
     return tuple(read_string(file) for _ in range(num))
 
-
 def read_float(file):
-    return struct.unpack_from('<f', file.read(4))[0]
-
+    return _read_single(file, "f")
 
 def read_floats(file, num):
-    return struct.unpack_from('<' + 'f' * num, file.read(4 * num))
-
+    return _read_multiple(file, "f", num)
 
 def read_uint(file):
-    return struct.unpack_from('<I', file.read(4))[0]
-
+    return _read_single(file, "I")
 
 def read_uints(file, num):
-    return struct.unpack_from('<' + 'I' * num, file.read(4 * num))
-
+    return _read_multiple(file, "I", num)
 
 def read_ushorts(file, num):
-    return struct.unpack_from('<' + 'H' * num, file.read(2 * num))
-
+    return _read_multiple(file, "H", num)
 
 def read_uchars(file, num):
-    return struct.unpack_from('<' + 'B' * num, file.read(num))
+    return _read_multiple(file, "B", num)
+
+def _read_single(file, format):
+    size = {
+        "f": 4,
+        "I": 4,
+        "H": 2,
+        "B": 1
+    }[format]
+    return struct.unpack("<" + format, file.read(size))[0]
+
+def _read_multiple(file, format, num):
+    return tuple(_read_single(file, format) for _ in range(num))
 
 
 class Mesh:
     def __init__(self, submeshes):
         self._submeshes = submeshes
 
-    def export_to_obj(self, filepath):
-        with open(filepath, 'w') as f:
-            f.write(f"# Exported OBJ file from Mesh class\n")
+    def export_to_obj(self, obj_file, mtl_file, mtl_filepath):
+        obj_file.write(f"# Exported from RW:ED\n")
 
-            # Write reference to the material file (.mtl)
-            mtl_filepath = filepath.replace(".obj", ".mtl")
-            f.write(f"mtllib {os.path.basename(mtl_filepath)}\n")
+        # Write reference to the material file (.mtl)
+        obj_file.write(f"mtllib {os.path.basename(mtl_filepath)}\n")
 
-            # Index counters for vertices, texture coords, and normals (OBJ indices are 1-based)
-            vertex_offset = 1
+        # Index counters for vertices, texture coords, and normals (OBJ indices are 1-based)
+        vertex_offset = 1
+        
+        for submesh in self._submeshes:
+            material, spec_map, positions, normals, colours, uvs, indices = submesh
+
+            # Use the material from the material library
+            obj_file.write(f"\nusemtl {material}\n")
             
-            for submesh in self._submeshes:
-                material, spec_map, positions, normals, colours, uvs, indices = submesh
+            # Write a comment with the material name
+            obj_file.write(f"\n# Material: {material}\n")
+            
+            # Write vertex positions
+            for position in positions:
+                obj_file.write(f"v {position[0]} {position[1]} {position[2]}\n")
+            
+            # Write texture coordinates (if present)
+            for uv in uvs:
+                obj_file.write(f"vt {uv[0]} {uv[1]}\n")
+            
+            # Write vertex normals (if present)
+            for normal in normals:
+                obj_file.write(f"vn {normal[0]} {normal[1]} {normal[2]}\n")
 
-                # Use the material from the material library
-                f.write(f"\nusemtl {material}\n")
-                
-                # Write a comment with the material name
-                f.write(f"\n# Material: {material}\n")
-                
-                # Write vertex positions
-                for position in positions:
-                    f.write(f"v {position[0]} {position[1]} {position[2]}\n")
-                
-                # Write texture coordinates (if present)
-                for uv in uvs:
-                    f.write(f"vt {uv[0]} {uv[1]}\n")
-                
-                # Write vertex normals (if present)
-                for normal in normals:
-                    f.write(f"vn {normal[0]} {normal[1]} {normal[2]}\n")
+            # Write face indices (OBJ is 1-based index)
+            for i in range(0, len(indices), 3):
+                # The face format in OBJ: f v1/vt1/vn1 v2/vt2/vn2 v3/vt3/vn3
+                v1, v2, v3 = indices[i], indices[i+1], indices[i+2]
+                obj_file.write(f"f {v1+vertex_offset}/{v1+vertex_offset}/{v1+vertex_offset} "
+                        f"{v2+vertex_offset}/{v2+vertex_offset}/{v2+vertex_offset} "
+                        f"{v3+vertex_offset}/{v3+vertex_offset}/{v3+vertex_offset}\n")
 
-                # Write face indices (OBJ is 1-based index)
-                for i in range(0, len(indices), 3):
-                    # The face format in OBJ: f v1/vt1/vn1 v2/vt2/vn2 v3/vt3/vn3
-                    v1, v2, v3 = indices[i], indices[i+1], indices[i+2]
-                    f.write(f"f {v1+vertex_offset}/{v1+vertex_offset}/{v1+vertex_offset} "
-                            f"{v2+vertex_offset}/{v2+vertex_offset}/{v2+vertex_offset} "
-                            f"{v3+vertex_offset}/{v3+vertex_offset}/{v3+vertex_offset}\n")
-
-        print(f"Mesh successfully exported to {filepath}")
+        print(f"Mesh successfully exported")
 
         # Write the .mtl file
-        self.export_mtl(mtl_filepath)
+        self.export_mtl(mtl_file)
 
-    def export_mtl(self, filepath):
-        with open(filepath, 'w') as f:
-            f.write(f"# Exported MTL file from Mesh class\n")
-            
-            for submesh in self._submeshes:
-                material, spec_map, positions, normals, colours, uvs, indices = submesh
-                
-                # Write material properties (for now, we assume simple properties)
-                f.write(f"\nnewmtl {material}\n")
-                f.write("Ka 1.000 1.000 1.000\n")  # Ambient color (white)
-                f.write("Kd 1.000 1.000 1.000\n")  # Diffuse color (white)
-                f.write("Ks 0.000 0.000 0.000\n")  # Specular color (black)
-                f.write("d 1.0\n")  # Transparency (opaque)
-                f.write("illum 2\n")  # Illumination model (default)
-                f.write(f"map_Kd {material}.tga\n")  # Diffuse texture
-
-                # Check if we have a specular map (assuming spec_map exists in your material)
-                if spec_map:
-                    f.write(f"map_Ks {spec_map}.tga\n")  # Specular map
+    def export_mtl(self, f):
+        f.write(f"# Exported from RW:ED\n")
         
-        print(f"Materials successfully exported to {filepath}")
+        for submesh in self._submeshes:
+            material, spec_map, positions, normals, colours, uvs, indices = submesh
+            
+            # Write material properties (for now, we assume simple properties)
+            f.write(f"\nnewmtl {material}\n")
+            f.write("Ka 1.000 1.000 1.000\n")  # Ambient color (white)
+            f.write("Kd 1.000 1.000 1.000\n")  # Diffuse color (white)
+            f.write("Ks 0.000 0.000 0.000\n")  # Specular color (black)
+            f.write("d 1.0\n")  # Transparency (opaque)
+            f.write("illum 2\n")  # Illumination model (default)
+            f.write(f"map_Kd {material}.tga\n")  # Diffuse texture
+
+            # Check if we have a specular map (assuming spec_map exists in your material)
+            if spec_map:
+                f.write(f"map_Ks {spec_map}.tga\n")  # Specular map
+        
+        print(f"Materials successfully exported")
+
+    def export_to_rwm(f):
+        f.write(b"MSH\x01")
+
 
     @classmethod
     def read_from_rwm_file(cls, f):
@@ -128,6 +134,7 @@ class Mesh:
             f.read(0x44)  # Who cares
 
         materials = []
+        spec_maps = {}
         for _ in range(num_materials):
             material = read_string(f)
             print(f"\nMaterial: {material}")
@@ -135,6 +142,7 @@ class Mesh:
             spec_map = read_string(f)  # The specular map may be an empty string if not present
             if spec_map:
                 print(f"Specular map: {spec_map}")
+                spec_maps[material] = spec_map
             print(f"Bitfield maybe: {f.read(4)}")
             print(f"No idea: {read_float(f)}")
             print(f"Another number (flags?): {read_uint(f)}")
@@ -160,7 +168,7 @@ class Mesh:
             num_tris = read_uint(f)
             print(f"Number of triangles: {num_tris}")
             indices = read_ushorts(f, num_tris*3)
-            submeshes.append((material, spec_map, positions, normals, colours, uvs, indices))
+            submeshes.append((material, spec_maps.get(material), positions, normals, colours, uvs, indices))
 
         print(f"\nFinished at {hex(f.tell())}")
         next_part = f.read(4)
@@ -168,11 +176,78 @@ class Mesh:
         assert next_part == b"DYS\x01" or not next_part
 
         return Mesh(submeshes)
+    
+    @classmethod
+    def read_from_obj_file(cls, f):
+        submeshes = []
+        current_mat = None
+        current_spec_map = None
+        current_positions = []
+        current_normals = []
+        current_colours = []
+        current_uvs = []
+        current_indices = []
+
+        def add_current_submesh():
+            assert len(current_positions) == len(current_normals) == len(current_uvs)
+            current_colours = [(1., 1., 1., 1.) for _ in current_positions]
+            submeshes.append((current_mat,
+                              current_spec_map,
+                              current_positions,
+                              current_normals,
+                              current_colours,
+                              current_uvs,
+                              current_indices))
+            submeshes = []
+            current_mat = None
+            current_spec_map = None
+            current_positions = []
+            current_normals = []
+            current_colours = []
+            current_uvs = []
+            current_indices = []
+
+        for line in f.readlines():
+            words = line.split()
+            if not words or words[0].startswith("#"):
+                continue
+
+            elif words[0] == "mtllib":
+                continue  # This is only useful for the spec map, so let's not bother for now
+
+            elif words[0] == "usemtl":
+                if current_mat:
+                    add_current_submesh()
+                current_mat = words[1]
+
+            elif words[0] == "f":
+                assert len(words) == 4 and all(v.split("/")[0] == v.split("/")[1] == v.split("/")[2] for v in words[1:])
+                for v in words[1:]:
+                    current_indices.append(int(v.split("/")[0]))
+
+            else:
+                thing = tuple(float(word) for word in words[1:])
+                {
+                    "v": current_positions,
+                    "vn": current_normals,
+                    "vt": current_uvs
+                }[words[0]].append(thing)
+
+        add_current_submesh()
+        return Mesh(submeshes)
+
 
 if __name__ == "__main__":
     in_path = sys.argv[1]
     print(f"Reading {in_path}".center(80, "="))
-    with open(in_path, "rb") as f:
-        m = Mesh.read_from_rwm_file(f)
-    out_path = os.path.splitext(in_path)[0] + ".obj"
-    m.export_to_obj(out_path)
+    basename, extension = os.path.splitext(in_path)
+    with open(in_path, "rb" if extension == ".rwm" else "r") as obj_file:
+        if extension == ".rwm":
+            m = Mesh.read_from_rwm_file(obj_file)
+            with open(basename + ".obj", 'w') as f1:
+                with open(basename + ".mtl", 'w') as f2:
+                    m.export_to_obj(f1, f2, basename + ".mtl")
+        else:
+            m = Mesh.read_from_obj_file(obj_file)
+            with open(basename + ".rwm", 'wb') as obj_file:
+                m.export_to_rwm(obj_file)
